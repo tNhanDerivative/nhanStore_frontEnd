28fbfdc009688e5050a5ec57b7562fc34bd8ff3d
tNhanDerivative <72671631+tNhanDerivative@users.noreply.github.com>
2023-06-22 19:02:26 +0700

    fix product page title

--- src/views/ProductView.vue
+++ src/views/ProductView.vue
@@ -57,7 +57,7 @@ export default {
                 .then(response => {
                     this.product = response.data
 
-                    document.title = this.product.name + ' | Djackets'
+                    document.title = this.product.name + ' | Vuejs demo'
                 })
                 .catch(error => {
                     console.log(error)
