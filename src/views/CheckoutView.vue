decd9299d1c0a0f3e3e64d90d9e47329cfa15231
tNhanDerivative <72671631+tNhanDerivative@users.noreply.github.com>
2023-06-22 18:23:32 +0700

    remove Stripe script for now

--- src/views/CheckoutView.vue
+++ src/views/CheckoutView.vue
@@ -118,7 +118,7 @@
 
 <script>
 import axios from 'axios'
-import {Stripe} from 'stripe'
+// import {Stripe} from 'stripe'
 
 export default {
     name: 'CheckoutView',
@@ -145,7 +145,7 @@ export default {
         this.cart = this.$store.state.cart
 
         if (this.cartTotalLength > 0) {
-            this.stripe = Stripe('pk_test_51H1HiuKBJV2qfWbD2gQe6aqanfw6Eyul5PO2KeOuSRlUMuaV4TxEtaQyzr9DbLITSZweL7XjK3p74swcGYrE2qEX00Hz7GmhMI')
+//            this.stripe = Stripe('pk_test_51H1HiuKBJV2qfWbD2gQe6aqanfw6Eyul5PO2KeOuSRlUMuaV4TxEtaQyzr9DbLITSZweL7XjK3p74swcGYrE2qEX00Hz7GmhMI')
             const elements = this.stripe.elements();
             this.card = elements.create('card', { hidePostalCode: true })
 
