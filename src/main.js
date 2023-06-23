72e8255a4b88dbea934d8861aeda65fd738db997
trongnhan <tnhanderivative@zohomail.com>
2023-06-23 18:20:47 +0700

    Apply .env.local file

--- src/main.js
+++ src/main.js
@@ -4,6 +4,6 @@ import router from './router'
 import store from './store'
 import axios from 'axios'
 
-axios.defaults.baseURL = 'apistore.nhan-thenoobmaster.com'
+axios.defaults.baseURL = process.env.VUE_APP_AXIOS_BASE_URL           //'apistore.nhan-thenoobmaster.com'
 
 createApp(App).use(store).use(router, axios).mount('#app')
