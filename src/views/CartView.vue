9c67dc8d6aba10976f6cb2846a362b075934e8e3
trongnhan <tnhanderivative@zohomail.com>
2023-07-18 01:12:10 +0700

    fix money currency

--- src/views/CartView.vue
+++ src/views/CartView.vue
@@ -32,7 +32,7 @@
             <div class="column is-12 box">
                 <h2 class="subtitle">Summary</h2>
 
-                <strong>${{ cartTotalPrice.toFixed(2) }}</strong>, {{ cartTotalLength }} items
+                <strong>đ{{ cartTotalPrice.toFixed(2) }}</strong>, {{ cartTotalLength }} items
 
                 <hr>
 
